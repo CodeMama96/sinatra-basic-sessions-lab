4bb2c6b998ef8b41fe5fb382893d977924fd29a7
Alicia Weenum <weenum.alicia@gmail.com>
2020-12-03 14:40:51 -0500

    commit tree 0e4d5a9291f1c97be339ed067070a3109444a08e

--- app.rb
+++ app.rb
@@ -1,4 +1,22 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    configure do
+        enable :sessions
+        set :session_secret, "secret"
+      end 
+
+    
+     get '/' do 
+        erb :index
+      end
+
+    post '/checkout' do
+      session[:item] = "socks" #key-value pair
+        @session = session
+        session
+      end
+
+
 end
